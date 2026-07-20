112c36ed8c3aae4527c82aac48c631fc7fe684ff
ravi1722 <sathyamravi2@gmail.com>
2026-07-20 20:27:19 +0530

    created services for restau

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -6,18 +6,60 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    protected $fillable = ['owner_id', 'name', 'slug', 'description', 'logo', 'cover_image', 'cuisine_type', 'phone', 'email', 'address', 'city', 'state', 'pincode', 'latitude', 'longitude', 'commission_percentage', 'minimum_order', 'delivery_time', 'delivery_fee', 'rating', 'total_reviews', 'status', 'is_open', 'is_featured'];
+    protected $fillable = [
+        'owner_id',
+        'name',
+        'slug',
+        'description',
+        'logo',
+        'cover_image',
+        'cuisine_type',
+        'phone',
+        'email',
+        'address',
+        'city',
+        'state',
+        'pincode',
+        'latitude',
+        'longitude',
+        'commission_percentage',
+        'minimum_order',
+        'delivery_time',
+        'delivery_fee',
+        'rating',
+        'total_reviews',
+        'status',
+        'is_open',
+        'is_featured'
+    ];
 
     public function owner()
     {
         return $this->belongsTo(User::class, 'owner_id');
     }
 
-    public function orders() {
+    public function menuCategories(){
+        return $this->hasMany(MenuCategory::class);
+    }
+
+    public function orders()
+    {
         return $this->hasMany(Order::class);
     }
 
-    public function reviews() {
+    public function reviews()
+    {
         return $this->hasMany(Review::class, 'reviewable');
+    }
+
+    //------- scopes
+    public function scopeInCity($query, $city)
+    {
+        return $query->where('city', $city);
+    }
+
+    public function scopeFeatured($query)
+    {
+        return $query->where('is_featured', true);
     }
 }
