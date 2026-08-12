3963dbbdab0ee540cd345528a56984994ecfa16e
ravi1722 <sathyamravi2@gmail.com>
2026-08-12 08:43:06 +0530

    latest

---
+++ app/View/Composers/RestaurantOwnerSidebarComposer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\View\Composers;
+
+class RestaurantOwnerSidebarComposer
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
